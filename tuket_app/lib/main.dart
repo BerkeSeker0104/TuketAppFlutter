5c205b2d3568a63ce67a54e3398fa17359ae4b01
Berke Seker <berkeseker.2000@gmail.com>
2025-03-01 21:36:46 +0300

    Firebase kurulumu ve entegrasyonu yapıldı

--- tuket_app/lib/main.dart
+++ tuket_app/lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:tuket_app/providers/auth_provider.dart';
 import 'package:tuket_app/screens/login_screen.dart';
 import 'package:tuket_app/screens/register_screen.dart';
 import 'package:tuket_app/screens/home_screen.dart';
 import 'package:tuket_app/screens/splash_screen.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     MultiProvider(
       providers: [
